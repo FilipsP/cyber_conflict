b01067cf2ba9a32251e8d2999ee263c419f99705
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2023-11-02 17:53:18 +0200

    Add simple navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,27 @@
-function Navbar() {
+import { Link } from "react-router-dom"
 
+function Navbar() {
+  return (
+    <nav className="w-full top-0 text-2xl bg-slate-200">
+      <div className="h-16 flex justify-between items-center mx-4">
+        <div>Menu</div>
+        <ul className="md:flex md-justify-end md-border-0 md:mr-8 md:space-x-8">
+          <Link
+            to="/"
+            className="hover:text-slate-500 transition duration-100 ease-out"
+          >
+            Game
+          </Link>
+          <Link
+            to="/docs"
+            className="hover:text-slate-500 transition duration-100 ease-out"
+          >
+            Docs
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
