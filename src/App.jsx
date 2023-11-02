fdfbed034cba81edceafa151935eea981da2ecd3
Filipp-Artur Pljassunov <33956131+FilipsP@users.noreply.github.com>
2023-11-02 16:53:36 +0200

    Initial react+vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1>Cyber conflict</h1>
+    </div>
+  )
+}
+
+export default App
--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-import * as PIXI from "pixi.js"
-
-const app = new PIXI.Application({
-  width: 800,
-  height: 600,
-  backgroundColor: 0x1099bb,
-})
-
-document.body.appendChild(app.view)
