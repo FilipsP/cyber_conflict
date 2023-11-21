80b60d8e7f7d2c87f96d6df8a80bdc684b0718f9
Mares1492 <msumarok@tlu.ee>
2023-11-21 02:59:55 +0200

    Add enemy economy data

--- src/js/classes/components/economyData.js
+++ src/js/classes/components/economyData.js
@@ -1,7 +1,12 @@
 import gameState from "../../gameState.js";
 
 export default class EconomyData {
-    constructor(parent) {
+    constructor(parent,who = "player") {
+        this.titleStyle = {
+            fontSize: 40,
+            fontFamily: 'Share Tech Mono',
+            color: '#D9D9D9',
+        }
         this.maxEconomy = 300
         this.maxSecurity = 300
         this.economy = this.maxEconomy * gameState.playerData.economy
@@ -9,11 +14,10 @@ export default class EconomyData {
         this.scaleFactor = 1
         this._setScaleFactor()
         this.parent = parent
-        this._buildEconomyData(parent)
+        this._buildEconomyData(parent,who)
     }
     _setScaleFactor(){
         const scale = window.innerHeight/1320
-        console.log(scale)
         if (scale<1){
             this.scaleFactor = scale
         }
@@ -21,17 +25,37 @@ export default class EconomyData {
             this.scaleFactor = 0.5
         }
     }
-    _buildEconomyData(parent){
+    _buildEconomyData(parent,who){
         this.background = parent.add.rectangle(0,0,600,350,0x21242A)
-        this.background.setStrokeStyle(3, 0x6BB9B6)
-        this.coin = parent.add.image(-this.background.width*0.30,-80,"coin")
-        this.shield = parent.add.image(-this.background.width*0.30,80,"shield")
-        this.economyBar = parent.add.rectangle(-60,-80,this.maxEconomy,100,0xFFD102).setOrigin(0,0.5)
-        this.economyBar.setStrokeStyle(3, 0xF2A203)
-        this.securityBar = parent.add.rectangle(-60,80,this.maxSecurity,100,0xA7ADBA).setOrigin(0,0.5)
-        this.securityBar.setStrokeStyle(3, 0x2F363D)
+        this.titleBackground = parent.add.rectangle(0,-250,250,80,0x21242A)
+        this.titleBackground.setStrokeStyle(5, 0x0C0A00)
+        if (who === "player"){
+            this.title = parent.add.text(this.titleBackground.x,this.titleBackground.y,"Your stats",this.titleStyle).setOrigin(0.5)
+            this.coin = parent.add.image(-this.background.width*0.30,-80,"coin")
+            this.shield = parent.add.image(-this.background.width*0.30,80,"shield")
+            this.background.setStrokeStyle(3, 0x6BB9B6)
+            this.economyBar = parent.add.rectangle(-60,-80,this.maxEconomy,100,0xFFD102).setOrigin(0,0.5)
+            this.economyBar.setStrokeStyle(3, 0xF2A203)
+            this.securityBar = parent.add.rectangle(-60,80,this.maxSecurity,100,0xA7ADBA).setOrigin(0,0.5)
+            this.securityBar.setStrokeStyle(3, 0x2F363D)
+
+        }
+        //TODO: else if/switch in case there can be more than two sides/types
+            //FF7A10 orange
+        else{
+            this.title = parent.add.text(this.titleBackground.x,this.titleBackground.y,"Enemy stats",this.titleStyle).setOrigin(0.5)
+            this.coin = parent.add.image(-this.background.width*0.30,-80,"blood_coin")
+            this.shield = parent.add.image(-this.background.width*0.30,80,"fire")
+            this.background.setStrokeStyle(3, 0xC10001)
+            this.economyBar = parent.add.rectangle(-60,-80,this.maxEconomy,100,0xFFD102).setOrigin(0,0.5)
+            this.economyBar.setStrokeStyle(3,0x400303)
+            this.securityBar = parent.add.rectangle(-60,80,this.maxSecurity,100,0xDA5E21).setOrigin(0,0.5)
+            this.securityBar.setStrokeStyle(3, 0xFE7F36)
+
+        }
         this.securityBar.width = this.security
-        this.container = parent.add.container(window.innerWidth/2, (window.innerHeight*0.6), [this.background,this.coin,this.shield,this.economyBar,this.securityBar]);
+        this.economyBar.width = this.economy
+        this.container = parent.add.container(window.innerWidth/2, (window.innerHeight*0.6), [this.background,this.titleBackground,this.title,this.coin,this.shield,this.economyBar,this.securityBar]);
         this.container.setScale(0)
     }
 
