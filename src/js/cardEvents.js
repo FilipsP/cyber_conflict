6464e8f637f5e2364bfebaa84cf2172aa70c5df7
Mares1492 <msumarok@tlu.ee>
2023-11-22 15:22:56 +0200

    Update cardEvents.js

--- src/js/cardEvents.js
+++ src/js/cardEvents.js
@@ -8,7 +8,7 @@ const cardEvents=[
             [{
                 title: "Attack “Orange”",
                 icon: "assets/objects/card/icons/troy.png",
-                body: "“Orange” is used to communicate with resistance forces in Patria, passing coded messages on military plans and targets",
+                body: "“Orange” is used to communicate with resistance forces in Patria, passing coded messages on military plans and targets.",
                 feedback:"According to Rule 38 “Orange” can qualify as a military objective as it is making an effective contribution to military action. Disabling “Orange” and with it communication between the adversary and forces aligned with them can offer us a definite military advantage. Thus, this action does not go against the rules.",
                 isLegal: true,
                 economy: 0,
@@ -27,7 +27,7 @@ const cardEvents=[
             economy: 0,
             security: 0,
             isLegal: false,
-            body: "We have captured Inimicus’ agent who has infiltrated our facility and installed spyware to collect information from our military networks. The agent has been arrested and the spyware neutralised. The enemy action is within the limits of international law, according to Rule 66 on cyber espionage, which states that information gathering does not violate the laws of armed conflict. According to this rule the captured agent is not considered as a prisoner of war, but as a spy"
+            body: "We have captured Inimicus’ agent who has infiltrated our facility and installed spyware to collect information from our military networks. The agent has been arrested and the spyware neutralised. The enemy action is within the limits of international law, according to Rule 66 on cyber espionage, which states that information gathering does not violate the laws of armed conflict. According to this rule the captured agent is not considered as a prisoner of war, but as a spy."
         }
     },
     {
@@ -40,8 +40,8 @@ const cardEvents=[
                 {
                     title: "Deploy “Jerry”",
                     icon: "assets/objects/card/icons/surgery.png",
-                    body: "“Jerry” is designed to identify a specific process-control system and disable it, shutting down closed network of specific weapons factory",
-                    feedback:"Even though the scale of “Jerry’s” effect is much smaller, its effect can be directed and limited to a legitimate military objective, thus not violating Rules 43 and 49 prohibiting indiscriminate attacks and Rule 54 on minimising incidental damage to civilian objects. This is a perfectly legal action from our side.",
+                    body: "“Jerry” is designed to identify a specific process-control system and disable it, shutting down closed network of specific weapons factory.",
+                    feedback:"Even though the scale of “Jerry’s” effect is much smaller, its effect can be directed and limited to a legitimate military objective, thus not violating Rules 43 and 49 prohibiting indiscriminate attacks and Rule 54 on minimising incidental damage to civilian objects. This is a legitimate action from our side.",
                     isLegal: true,
                     economy: 0,
                     security: 0.1
@@ -59,7 +59,7 @@ const cardEvents=[
             economy: 0,
             security: 0.1,
             isLegal: false,
-            body: "Inimicus has conducted a cyber attack on our railway network which was used for civilian and military transport, disrupting the traffic. Because it was a dual-use object, Inimicus’ actions do not violate international law. According to Rule 39 objects that are used for both civilian and military purposes, including the rail network, can be considered as a military objective. (although this is not always clear cut, see the proportionality principle)"
+            body: "Inimicus has conducted a cyberattack on our railway network which was used for civilian and military transport, disrupting the traffic. Because it was a dual-use object, Inimicus’ actions do not violate international law. According to Rule 39 objects that are used for both civilian and military purposes, including the rail network, can be considered as a military objective. (although this is not always clear cut, see the proportionality principle)."
         }
     },
     {
@@ -73,7 +73,7 @@ const cardEvents=[
                     title: "Attack Military Command",
                     icon: "assets/objects/card/icons/headshot.png",
                     body: "Attack the military command network directly, causing chaos and loss of control.",
-                    feedback:"As per rule 56, this is the appropriate action as it yields the same result as the first option, but with causing less danger to civilians",
+                    feedback:"As per rule 56, this is the appropriate action as it yields the same result as the first option, but with causing less danger to civilians.",
                     isLegal: true,
                     economy: 0,
                     security: 0.1
@@ -82,7 +82,7 @@ const cardEvents=[
                     title: "Attack dual-use system",
                     icon: "assets/objects/card/icons/dual.png",
                     body: "Disable Military/Civilian communications lines causing mass disruption and loss of communication possibly leading to the economical collapse of the enemy.",
-                    feedback:"According to rule 56, when two possible attack options will yield the same result, we should choose the one that causes least danger to civilians. In this case the same result could have been achieved with the second option, while not endangering civilians by disrupting their communication lines as well. If the international investigation also finds that we had other, more appropriate choice leading to similar military advantage our actions will be condemned with the possibility of imposing sanctions",
+                    feedback:"According to rule 56, when two possible attack options will yield the same result, we should choose the one that causes least danger to civilians. In this case the same result could have been achieved with the second option, while not endangering civilians by disrupting their communication lines as well. If the international investigation also finds that we had other, more appropriate choice leading to similar military advantage our actions will be condemned with the possibility of imposing sanctions.",
                     isLegal: false,
                     economy: -0.1,
                     security: 0.1
@@ -123,13 +123,13 @@ const cardEvents=[
             economy: -0.1,
             security: 0,
             isLegal: false,
-            body: "We were informed that Inimicus conducted a cyber attack against the government's cyber infrastructure. The attack also affected the digital archives of the Patria’s Film Museum, which contained unique files that have survived only in digital format after the fire a few years ago at the archive. This cultural heritage is now lost. Rule 82 of the Tallinn Manual calls on states to respect and protect cultural property. We will bring this incident to the attention of the international community and demand strict sanctions."
+            body: "We were informed that Inimicus conducted a cyberattack against the government's cyber infrastructure. The attack also affected the digital archives of the Patria’s Film Museum, which contained unique files that have survived only in digital format after the fire a few years ago at the archive. This cultural heritage is now lost. Rule 82 of the Tallinn Manual calls on states to respect and protect cultural property. We will bring this incident to the attention of the international community and demand strict sanctions."
         }
     },
     {
         task:{
             title: "Advisor:",
-            body : "We suspect the web-page doodle.org that provides services for educational institutions may be used by Inimicus for transmitting military communication. At the moment it is only a suspicion and would need additional time and resources to confirm. While we proceed with the confirmation, Inimicus may have time to set up different communication channels. How should we proceed?"
+            body : "We suspect the web-page doodle.org which provides services for educational institutions may be used by Inimicus for transmitting military communication. At the moment it is only a suspicion and would need additional time and resources to confirm. While we proceed with the confirmation, Inimicus may have time to set up different communication channels. How should we proceed?"
         },
         cards:
             [
@@ -137,7 +137,7 @@ const cardEvents=[
                     title: "Attack!",
                     icon: "assets/objects/card/icons/bombs.png",
                     body: "Conduct DDoS attack on doodle.org immediately, no time to waste!",
-                    feedback:"Rule 53 of the Tallinn Manual requires verification of targets when planning or ordering the attack by all feasible means, to make sure that objects attacked are not civilian. By refusing to take further steps for verification of the nature of the target we have violated this rule and may be subject to sanctions",
+                    feedback:"Rule 53 of the Tallinn Manual requires verification of targets when planning or ordering the attack by all feasible means, to make sure that objects attacked are not civilian. By refusing to take further steps for verification of the nature of the target we have violated this rule and may be subject to sanctions.",
                     isLegal: false,
                     economy: -0.1,
                     security: 0
@@ -145,8 +145,8 @@ const cardEvents=[
                 {
                     title: "Verify data",
                     icon: "assets/objects/card/icons/eye.png",
-                    body: "Verify if doodle.org is indeed used by State B for military purposes and proceed with the attack only after confirmation. We can lose advantage",
-                    feedback:"Doing everything feasible to verify that the targeted object is not a civilian object is the legal course of action and complies with the Rule 53 of the Tallinn Manual. Even though the verification process may take some time, this way we will be certain that the object is a legitimate target and our actions do not violate international law",
+                    body: "Verify if doodle.org is indeed used by Inimicus for military purposes and proceed with the attack only after confirmation. We can lose advantage",
+                    feedback:"Doing everything feasible to verify that the targeted object is not a civilian object is the legal course of action and complies with the Rule 53 of the Tallinn Manual. Even though the verification process may take some time, this way we will be certain that the object is a legitimate target and our actions do not violate international law.",
                     isLegal: true,
                     economy: 0,
                     security: 0
@@ -155,7 +155,7 @@ const cardEvents=[
             economy: -0.1,
             security: 0,
             isLegal: false,
-            body: "Inimicus’ General Pitt has threatened to attack the water filtration system of our capital city, claiming that they will contaminate the water supply, which can lead to widespread diseases and death in the civilian population. This statement is aiming to terrorise the civilian population, which goes against international law. Rule 36 prohibits cyber attacks or threats that are aimed at terrorising civilian population. We expect the international community to respond to this violation and sanction Inimicus accordingly."
+            body: "Inimicus’ General Pitt has threatened to attack the water filtration system of our capital city, claiming that they will contaminate the water supply, which can lead to widespread diseases and death in the civilian population. This statement is aiming to terrorize the civilian population, which goes against international law. Rule 36 prohibits cyberattacks or threats that are aimed at terrorizing the civilian population. We expect the international community to respond to this violation and sanction Inimicus accordingly."
         }
     },
     {
@@ -178,7 +178,7 @@ const cardEvents=[
                     title: "Capture the target",
                     icon: "assets/objects/card/icons/prison.png",
                     body: "Aim of this operation is to try and capture him but the chances of success are lower, as the operation would take place on the enemy territory and the General has a very skilled security team.",
-                    feedback:"Even though the chances of success of this operation are small, this is the legal choice of action. Rule 60 specifies, that perfidy (an act inviting confidence of the adversary in order to betray that confidence) leading to their injury or death is prohibited, but if we manage to capture General Pitt unharmed, Patria will not be violating international law",
+                    feedback:"Even though the chances of success of this operation are small, this is the legal choice of action. Rule 60 specifies, that perfidy (an act inviting confidence of the adversary in order to betray that confidence) leading to their injury or death is prohibited, but if we manage to capture General Pitt unharmed, Patria will not be violating international law.",
                     isLegal: true,
                     economy: 0,
                     security: 0
@@ -193,7 +193,7 @@ const cardEvents=[
     {
         task:{
             title: "Advisor:",
-            body : "Our team has been planning a cyber attack directed at a small enemy military unit occupying a strategic location in the bordering region, that will cause an explosion on the territory of the military base. However, updated intelligence shows that at the moment there is a large concentration of civilian population near this territory."
+            body : "Our team has been planning a cyberattack directed at a small enemy military unit occupying a strategic location in the bordering region, that will cause an explosion on the territory of the military base. However, updated intelligence shows that at the moment there is a large concentration of civilian population near this territory."
         },
         cards:
             [
@@ -219,7 +219,7 @@ const cardEvents=[
             economy: 0,
             security: 0.1,
             isLegal: true,
-            body: "Inimicus has conducted a cyber attack allowing them to take control over our guided weapons systems. One of the guided missiles aimed at the enemy military installation was redirected and fell on an open field, killing one bystander. Even though the cyber operation resulted in a civilian casualty, this attack may be considered proportional. Inimicus claims that there was no way of anticipating and preventing civilian death. Rules 51 and 58 of Tallinn Manual address the proportionality and requirement to take precaution during the attack. According to our investigation Inimicus has not violated international laws as the target was legitimate and did not violate rules 51 and 58."
+            body: "Inimicus has conducted a cyberattack allowing them to take control over our guided weapons systems. One of the guided missiles aimed at the enemy military installation was redirected and fell on an open field, killing one bystander. Even though the cyber operation resulted in a civilian casualty, this attack may be considered proportional. Inimicus claims that there was no way of anticipating and preventing civilian death. Rules 51 and 58 of Tallinn Manual address the proportionality and requirement to take precaution during the attack. According to our investigation Inimicus has not violated international laws as the target was legitimate and did not violate rules 51 and 58."
         }
     },
     {
@@ -251,7 +251,7 @@ const cardEvents=[
             economy: -0.2,
             security: 0,
             isLegal: false,
-            body: "We have received information that State B has conducted a cyber attack that affected the power supply of the main hospital where dozens of critically injured civilians were being treated on life support. State B claims that medical computer systems were used for non-humanitarian purposes, but have not provided proof. State B has also not provided warning about the loss of protection and reasonable time limit for compliance, violating Rule 73 of the Tallinn Manual on loss of protection and warning. Their illegal action has resulted in severe international sanctions from the international community crippling their economy."
+            body: "We have received information that State B has conducted a cyberattack that affected the power supply of the main hospital where dozens of critically injured civilians were being treated on life support. State B claims that medical computer systems were used for non-humanitarian purposes, but have not provided proof. State B has also not provided warning about the loss of protection and reasonable time limit for compliance, violating Rule 73 of the Tallinn Manual on loss of protection and warning. Their illegal action has resulted in severe international sanctions from the international community crippling their economy."
         }
     }
 ]
